a86b885da388e178c5c7b03c5f1e4377ae9f87b1
Faith Letsoara <faithletsoara@gmail.com>
2023-04-20 12:21:58 -0700

    Hello printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  A program that prints a line with puts function
+ * Return: 0 (Success)
+ * /
+ * int	
+int main(void)
+{
+puts("\"Programming is like a multiligual puzzle");
+return (0);
+}
