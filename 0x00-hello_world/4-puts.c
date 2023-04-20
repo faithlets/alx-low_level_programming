295e4d2bbad3ed27925114e6aa81f98c6d4c6d6e
Faith Letsoara <faithletsoara@gmail.com>
2023-04-20 13:06:01 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - A program that prints a line with puts function
-* Return: 0(Success)
-* /
-* int main(void)
+ * main - A program that prints a line with put functions
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-puts("\"Programming is like a multiligual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
