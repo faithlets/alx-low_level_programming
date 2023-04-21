5c315e6fdededcfecdda7d262f1a2098c6d4db28
Faith Letsoara <faithletsoara@gmail.com>
2023-04-21 16:25:53 -0700

    alphABETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+
 /**
  * main - Prints and alphabet.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int i;
-
-        for (i = 0; i < 52; i++)
-        {
-                putchar(alp[i]);
-        }
-        putchar('\n');
-        return (0);
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
 }
