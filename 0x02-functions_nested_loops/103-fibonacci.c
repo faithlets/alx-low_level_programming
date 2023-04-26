a4668568927f9e2f1c7fab56072f6e98158e941a
Faith Letsoara <faithletsoara@gmail.com>
2023-04-26 12:17:16 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the even-valued terms
- * followed by a new line
- * Return: Always 0 (Success)
+ * main - Prints the sum of even fibonacci numbers
+ * less than 4000000.
+ * Return: Nothing!
  */
 
 int main(void)
 {
-	int i
-	unsigned long int j, k, next, sum;
+	int i = 0;
+	long j = 1, k = 2, sum = k;
 
-	j = 1;
-	k = 2;
-	sum = 0;
-	for (i = 1; i <= 33; ++i)
+	while (k + j < 4000000)
 	{
-	if (j < 4000000 && (j % 2) == 0)
-	{
-	sum = sum + j;
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	j = k - j;
+	++i;
 	}
-	next = j + k;
-	j = k;
-	k = next;
-	}
-	printf("%lu\n", sum);
+	printf("%ld\n", sum);
 	return (0);
 }
